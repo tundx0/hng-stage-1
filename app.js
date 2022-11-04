12fa00e61f3affb36ec4dab7193f2f7e27de7167
unknown <tundx0@gmail.com>
2022-11-04 15:34:36 +0100

    change added new operation type

--- app.js
+++ app.js
@@ -9,29 +9,38 @@ const PORT = process.env.PORT || 3030
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}...`)
 })
-
-app.get('/', (req, res) => {
-  res.json({
-  
+const myData = {
     slackUsername: 'tundx0',
     backend: true,
     age: 23,
     bio: 'I am a Software Engineer interested in building amazing software'
-  })
+  }
+// const add = (x, y) => {
+//   Number(x) + Number(y)
+// }
+app.get('/', (req, res) => {
+  res.json(myData)
 })
 
 app.post('/operand', (req, res) => {
   try{
-  const body = req.body
-  // console.log(typeof body.operation_type)
-  if(body.operation_type === 'addition'){
-    result = Number(body.x) + Number(body.y)
-    res.json({
-    slackUsername: 'Tundx0',
-    operation_type: 'addition',
-    result: result
-  })
-  }
+    const body = req.body
+    // console.log(typeof body.operation_type)
+    if(body.operation_type === 'addition'){
+      result = Number(body.x) + Number(body.y)
+      res.json({
+      slackUsername: myData.slackUsername,
+      operation_type: body.operation_type,
+      result: result
+      })
+    }else if(body.operation_type === 'subtraction'){
+            result = Number(body.x) - Number(body.y)
+            res.json({
+            slackUsername: myData.slackUsername,
+            operation_type: body.operation_type,
+            result: result
+            })
+    }
   } catch {
     res.json({
       status: res.statusCode,
@@ -39,6 +48,10 @@ app.post('/operand', (req, res) => {
 
     })
   }
+
+
+
+
 
   // console.log(result)
 
